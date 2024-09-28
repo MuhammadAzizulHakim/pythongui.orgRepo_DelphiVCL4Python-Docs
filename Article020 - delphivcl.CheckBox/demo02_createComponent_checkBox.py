7dc3cd239164962de3ada857f7d8297398c26e17
Muhammad Azizul Hakim <19azizul92@gmail.com>
2024-09-28 08:25:03 +0700

    Add files via upload

---
+++ Article020 - delphivcl.CheckBox/demo02_createComponent_checkBox.py
@@ -0,0 +1,83 @@
+from delphivcl import *
+
+# Create a Class to build a basic Form
+class MainForm(Form):
+    def __init__(self, Owner):
+        self.Caption = "Introduction to VCL Components"
+        self.Name = "BaseForm"
+        self.SetBounds(10, 10, 500, 450)
+
+        # Create a Main Panel component
+        pnlMain = CreateComponent('TPanel',Owner)
+        pnlMain.SetProps(Parent=self, Caption="",align = "alClient", Name = "MainPanel")
+
+        # Page control creation
+        pgConMain = PageControl(pnlMain)
+        pgConMain.Name = "MyPageControl"
+        pgConMain.Parent = pnlMain
+        pgConMain.Align = "alClient"
+
+        # Tabsheet one
+        pgOne = TabSheet(pnlMain)
+        pgOne.PageControl = pgConMain
+        pgOne.Caption = "Tab 1"
+
+        # Tabsheet two
+        pgTwo = TabSheet(pnlMain)
+        pgTwo.PageControl = pgConMain
+        pgTwo.Caption = "Tab 2"
+
+        # Label creation
+        lblHello = CreateComponent('TLabel',pgOne)
+        lblHello.Parent = pgOne
+        lblHello.Caption = 'FirstName'
+        lblHello.Name = 'Name'
+        # Set the positions
+        lblHello.Left = 20
+        lblHello.Top = 14
+        lblHello.Width = 121
+        lblHello.Height = 30
+
+        # Edit box creation
+        edtHello = CreateComponent('TEdit',pgOne)
+        edtHello.Parent = pgOne
+        edtHello.Text = 'Arthur'
+        edtHello.Name = 'edtFirstName'
+        # Set the positions and dimensions
+        edtHello.Left = 145
+        edtHello.Top = 14
+        edtHello.Width = 121
+        edtHello.Height = 30
+
+        # Check box creation
+        chkSingle = CheckBox(self)
+        chkSingle.Parent = pgOne
+        chkSingle.Caption = 'Single ?'
+        chkSingle.Alignment = 'taLeftJustify'
+        chkSingle.SetBounds(20, 44, 141, 30)
+
+        # label creation
+        lblCountry = CreateComponent('TLabel',pgOne)
+        lblCountry.SetProps(Parent=pgOne,Caption='Country')
+        lblCountry.SetBounds(20,88,121,30)
+
+        # List box creation
+        lboxCountry = ListBox(pgOne)
+        lboxCountry.SetProps(Parent=pgOne)
+        lboxCountry.SetBounds(145,88,121,60)
+        lboxCountry.Items.Add('RUSSIA')
+        lboxCountry.Items.Add('USA')
+        lboxCountry.Items.Add('INDIA')
+        lboxCountry.Items.Add('AUSTRALIA')
+
+# Initialize your application
+def main():
+    Application.Initialize()
+    Application.Title = "MyDelphiApp"
+    f = MainForm(Application)
+    f.Show()
+    FreeConsole()
+    Application.Run()
+    Application.Destroy()
+
+main()
