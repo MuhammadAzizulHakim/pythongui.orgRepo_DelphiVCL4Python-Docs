ec36157da54d0dfbb714e2b7cf32d9a99c6e45c1
Muhammad Azizul Hakim <19azizul92@gmail.com>
2024-11-15 22:20:27 +0700

    Add files via upload

---
+++ Article024 - delphivcl.Button/demo02_createButton.py
@@ -0,0 +1,41 @@
+from delphivcl import *
+
+# Create a Class to build a basic Form
+class MainForm(Form):
+    def __init__(self, Owner):
+        self.Caption = "Introduction to VCL Components"
+        self.Name = "BaseForm"
+        self.SetBounds(10, 10, 500, 450)
+
+        # Create a Main Panel component
+        pnlMain = CreateComponent('TPanel',Owner)
+        pnlMain.SetProps(Parent=self, Caption="",align = "alClient", Name = "MainPanel")
+
+        # Page control creation
+        pgConMain = PageControl(pnlMain)
+        pgConMain.Name = "MyPageControl"
+        pgConMain.Parent = pnlMain
+        pgConMain.Align = "alClient"
+
+        # Tabsheet one
+        pgOne = TabSheet(pnlMain)
+        pgOne.PageControl = pgConMain
+        pgOne.Caption = "Tab 1"
+
+        #Button
+        btnOK = Button(pgOne)
+        btnOK.SetProps(Parent=pgOne,Caption = 'Submit',Name = 'btnOK')
+        btnOK.SetBounds(145,50,60,30)
+
+
+# Initialize your application
+def main():
+    Application.Initialize()
+    Application.Title = "MyDelphiApp"
+    f = MainForm(Application)
+    f.Show()
+    FreeConsole()
+    Application.Run()
+    Application.Destroy()
+
+main()
